affc6c70ca029a834865b081fa845a9d46a7ef43
bryanhuynh <bryan.huynh1@ucalgary.ca>
2021-07-06 23:17:50 -0600

    post that are removed are re-rendered instead of refreshing page. TODO move this handle of the post deletion to the parent

--- client/src/components/Posts/Post/index.js
+++ client/src/components/Posts/Post/index.js
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useStyles from './styles'
 import { deletePost } from '../../../actions/posts'
-import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import {Card, CardActions, CardHeader, CardMedia, Button, Typography} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 
 const Post = ({post}) => {
     const classes = useStyles();
-    return(
-        <>  
-        <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
-            <div className={classes.overlay}>
-                <Typography variant="h6">{post.creator}</Typography>
-            </div>
-            <div className={classes.overlay2}>
-                <Button style={{color: 'white'}} size='small' onClick={() => {}}>
-                    <MoreHorizIcon fontSize="default" />
-                </Button>
-            </div>
-            <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() =>{deletePost(post._id)}}>
-                    <DeleteIcon fontSize="small"/>
-                    Delete
-                </Button>
-                <Typography variant="h6">{moment(post.createdAt).fromNow()}</Typography>
-            </CardActions>
-        </Card>
+    const [postStatus, setPostStatus] = useState(true);
 
-        </>
+    const handleDelete = (id) => {
+        setPostStatus(false);
+        deletePost(id)
+    }
+
+    const render = () => {
+        if(postStatus){
+            return (
+                <>  
+                <Card className={classes.card}>
+                    <CardHeader
+                        title={post.title}
+                        subheader={moment(post.createdAt).fromNow()}
+                    />
+                        
+                    <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+                    <CardActions className={classes.cardActions}>
+                        <Button size="small" color="primary" onClick={() =>{handleDelete(post._id)}}>
+                            <DeleteIcon fontSize="small"/>
+                            Delete
+                        </Button>
+                    </CardActions>
+                </Card>
+        
+                </>
+            )
+        }
+        return (
+            <>
+            </>
+        )
+
+    }
+    return(
+        render()
     ) 
 }
 
